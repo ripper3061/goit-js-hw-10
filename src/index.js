79ce3063ec122cd8d24a1866f534fd11df168e0a
Vladimir Ponomarov <ripper3061@gmail.com>
2022-09-04 13:21:30 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const countryCard = document.querySelector('.country-info');
+
+fetch(
+  'https://restcountries.com/v3.1/name/peru?fields=name,capital,population,flags,languages'
+)
+  .then(res => {
+    return res.json();
+  })
+  .then(country => {
+    console.log(country);
+    const { population } = country;
+    let cardMarkup = `<div class='card'>
+                        <div class='country-name'>
+                            <p class='country-name-text'>${population}</p>
+                        </div>
+                    </div>`;
+    countryCard.innerHTML = cardMarkup;
+  });
